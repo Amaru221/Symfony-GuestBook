91841f145f54c3c8dd418dd89e07011542eaff39
Amaru221 <agd1923gmail.com>
2024-11-22 11:32:51 +0100

    Include Readme.  Install composer package pinky, css-selector, twig/inky-extra. Config mailer and workflow.  CommentMessageHandler updated for notifications mail. Configure services.yaml parameters: email, base_url. Added templates for mails. Created AdminController to accept or reject comments across the mail

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Symfony-GuestBook
+
+Esta aplicación web es un libro de visitas online de conferencias
+
+Instalación
+
+1. Instalar dependencias de composer
+$> composer install
+
+2. lanzar aplicación symfony en segundo plano
+$> symfony server:start -d
+
+3. Crear contenedores docker para servidor, blackfire y mailer
+$> docker compose up -d
--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -3,10 +3,25 @@
 namespace App\MessageHandler;
 
 use App\SpamChecker;
+use Psr\Log\LoggerInterface;
 use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+/**
+ * Handler for processing CommentMessage objects.
+ * 
+ * This class is responsible for managing the lifecycle of comments,
+ * including spam checking, state transitions, and persistence.
+ * It uses a workflow to manage the comment's state and dispatches
+ * messages for further processing when necessary.
+ */
 
 #[AsMessageHandler]
 class CommentMessageHandler{
@@ -14,6 +29,11 @@ class CommentMessageHandler{
         private EntityManagerInterface $em,
         private SpamChecker $spamChecker,
         private CommentRepository $commentRepository,
+        private MessageBusInterface $bus,
+        private WorkflowInterface $commentStateMachine,
+        private MailerInterface $mailer,
+        #[Autowire('%admin_email%')] private string $adminEmail,
+        private ?LoggerInterface $logger = null,
     )
     {
         
@@ -26,16 +46,33 @@ class CommentMessageHandler{
             return;
         }
 
-        if(2 === $this->spamChecker->getSpamScore($comment, $message->getContext())){
-            $comment->setState('spam');
-        
-        }else{
+        if($this->commentStateMachine->can($comment, 'accept')){
+            $score = $this->spamChecker->getSpamScore($comment, $message->getContext());
             
-            $comment->setState('published');
+            $transition = match ($score){
+                2 => 'spam',
+                1 => 'might_be_spam',
+                default => 'accept',
+            };
 
+            $this->commentStateMachine->apply($comment, $transition);
+            $this->em->flush();
+            $this->bus->dispatch($message);
+        }elseif($this->commentStateMachine->can($comment, 'publish')||$this->commentStateMachine->can($comment, 'publish_ham')){
+            // $this->commentStateMachine->apply($comment, $this->commentStateMachine->can($comment, 'publish') ? 'publish' : 'publish_ham');
+            // $this->em->flush();
+
+            $this->mailer->send((new NotificationEmail())
+                ->subject('New comment posted')
+                ->htmlTemplate('emails/comment_notification.html.twig')
+                ->from($this->adminEmail)
+                ->to($this->adminEmail)
+                ->context(['comment' => $comment])
+                );
+        }elseif($this->logger){
+            $this->logger->debug('Dropping the comment message', ['comment' => $comment->getId(), 'state' => $comment->getState()]);
         }
         
-        $this->em->flush();
     
     }
 
